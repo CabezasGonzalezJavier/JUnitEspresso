42fbcbf3d68f73969e2e74fd9bf602d41d8933cc
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-09-25 14:47:58 +0100

    added capture in test

--- app/src/test/java/com/dream/myself/NotesPresenterTest.java
+++ app/src/test/java/com/dream/myself/NotesPresenterTest.java
@@ -4,12 +4,19 @@ import com.dream.myself.data.Note;
 import com.dream.myself.data.NotesRepository;
 import com.dream.myself.notes.NotesContract;
 import com.dream.myself.notes.NotesPresenter;
+import com.google.common.collect.Lists;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
@@ -20,22 +27,30 @@ import static org.mockito.Mockito.verify;
 
 public class NotesPresenterTest {
 
+    private static List<Note> NOTES;
+
+    private static List<Note> EMPTY_NOTES;
+
     @Mock
     NotesRepository mNotesRepository;
 
     @Mock
     NotesContract.View mNotesView;
 
-    NotesPresenter mPresenter;
+    @Captor
+    private ArgumentCaptor<NotesRepository.LoadNotesCallback> mLoadNotesCallbackCaptor;
 
+    NotesPresenter mPresenter;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        NOTES = Lists.newArrayList(new Note("Title1", "Description1"),
+                new Note("Title2", "Description2"));
+        EMPTY_NOTES = new ArrayList<>(0);
 
         mPresenter = new NotesPresenter(mNotesRepository, mNotesView);
     }
-
 
     @Test
     public void addNewNoteTest() {
@@ -43,7 +58,6 @@ public class NotesPresenterTest {
         mPresenter.addNewNote();
 
         verify(mNotesView).showAddNote();
-
     }
 
     @Test
@@ -51,13 +65,27 @@ public class NotesPresenterTest {
 
         String title = "title";
         String description = "description";
-        Note note = new Note(title, description;
+        Note note = new Note(title, description);
 
         mPresenter.openNoteDetails(note);
-
 
         verify(mNotesView).showNoteDetailUi(any(String.class));
     }
 
+    @Test
+    public void loadNotesTest() {
 
+        boolean forceUpdate = true;
+
+        mPresenter.loadNotes(forceUpdate);
+
+        verify(mNotesRepository).getNotes(mLoadNotesCallbackCaptor.capture());
+        mLoadNotesCallbackCaptor.getValue().onNotesLoaded(NOTES);
+
+        InOrder inOrder = Mockito.inOrder(mNotesView);
+        inOrder.verify(mNotesView).setProgressIndicator(true);
+        inOrder.verify(mNotesView).setProgressIndicator(false);
+        verify(mNotesView).showNotes(NOTES);
+
+    }
 }
