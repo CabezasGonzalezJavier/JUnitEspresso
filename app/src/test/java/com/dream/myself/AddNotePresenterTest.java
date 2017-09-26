80d18b511b34021010260479095ad7be9244dbc8
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-09-26 14:50:17 +0100

    added more test

--- app/src/test/java/com/dream/myself/AddNotePresenterTest.java
+++ app/src/test/java/com/dream/myself/AddNotePresenterTest.java
@@ -11,8 +11,15 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.contains;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by javiergonzalezcabezas on 24/9/17.
@@ -40,7 +47,7 @@ public class AddNotePresenterTest {
     }
 
     @Test
-    public void saveNote() {
+    public void saveNote_showsSuccessMessageUi() {
         String title = "title";
         String description = "description";
 
@@ -48,5 +55,58 @@ public class AddNotePresenterTest {
 
         verify(mNotesRepository).saveNote(any(Note.class));
         verify(mAddNoteView).showNotesList();
+    }
+
+    @Test
+    public void saveNote_emptyNoteShowsErrorUi() {
+
+    }
+
+    @Test
+    public void imageAvailable_SavesImageAndUpdatesUiWithThumbnail() {
+        // Given an a stubbed image file
+        String imageUrl = "path/to/file";
+        when(mImageFile.exists()).thenReturn(true);
+        when(mImageFile.getPath()).thenReturn(imageUrl);
+
+        mAddNotesPresenter.imageAvailable();
+
+        verify(mAddNoteView).showImagePreview(contains(imageUrl));
+
+    }
+
+    @Test
+    public void imageAvailable_FileDoesNotExistShowsErrorUi() {
+        when(mImageFile.exists()).thenReturn(false);
+
+        mAddNotesPresenter.imageAvailable();
+
+
+        verify(mImageFile).delete();
+        verify(mAddNoteView).showImageError();
+
+    }
+
+    @Test
+    public void noImageAvailable_ShowsErrorUi() {
+
+        mAddNotesPresenter.imageCaptureFailed();
+
+        verify(mImageFile).delete();
+        verify(mAddNoteView).showImageError();
+    }
+
+    @Test
+    public void takePicture_CreatesFileAndOpensCamera() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        mAddNotesPresenter.takePicture();
+
+
+        verify(mImageFile).create(contains(imageFileName),anyString());
+        verify(mAddNoteView).openCamera(anyString());
+
     }
 }
