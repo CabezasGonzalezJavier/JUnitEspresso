be684e8248d0195bbfe36a0200c3da813beac3d7
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-10-20 15:43:15 +0100

    added more test

--- app/src/test/java/com/dream/myself/InMemoryNotesRepositoryTest.java
+++ app/src/test/java/com/dream/myself/InMemoryNotesRepositoryTest.java
@@ -1,72 +1,8 @@
 package com.dream.myself;
 
-import com.dream.myself.data.InMemoryNotesRepository;
-import com.dream.myself.data.Note;
-import com.dream.myself.data.NotesRepository;
-import com.dream.myself.data.NotesServiceApi;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isNull;
-import static org.mockito.Mockito.verify;
-
 /**
- * Created by javierg on 19/10/2017.
+ * Created by javierg on 20/10/2017.
  */
 
 public class InMemoryNotesRepositoryTest {
-
-    @Mock
-    private NotesServiceApi mNotesServiceApi;
-
-    @Mock
-    private NotesRepository.LoadNotesCallback mLoadNotesCallback;
-
-    @Mock
-    private NotesRepository.GetNoteCallback mGetNoteCallback;
-
-    InMemoryNotesRepository mInMemoryNotesRepository;
-
-    @Before
-    public void setUpInMemoryNotesRepositoryTest() {
-        MockitoAnnotations.initMocks(this);
-
-        mInMemoryNotesRepository = new InMemoryNotesRepository(mNotesServiceApi);
-    }
-
-    @Test
-    public void getNotes_onNotesLoaded() {
-
-        mInMemoryNotesRepository.getNotes(mLoadNotesCallback);
-
-        verify(mNotesServiceApi).getAllNotes(any(NotesServiceApi.NotesServiceCallback.class));
-    }
-
-    @Test
-    public void getNote() {
-
-        Note note = new Note("title", "description");
-
-        mInMemoryNotesRepository.getNote(note.getId(), mGetNoteCallback);
-
-        verify(mNotesServiceApi).getNote(eq(note.getId()), any(NotesServiceApi.NotesServiceCallback.class));
-    }
-
-    @Test
-    public void saveNote() {
-
-        Note note = new Note("title", "description");
-
-        mInMemoryNotesRepository.saveNote(note);
-
-        assertThat(mInMemoryNotesRepository.mCachedNotes, nullValue());
-    }
 }
--- app/src/test/java/com/dream/myself/NotesPresenterTest.java
+++ app/src/test/java/com/dream/myself/NotesPresenterTest.java
@@ -1,86 +1,8 @@
 package com.dream.myself;
 
-import com.dream.myself.data.Note;
-import com.dream.myself.data.NotesRepository;
-import com.dream.myself.notes.NotesContract;
-import com.dream.myself.notes.NotesPresenter;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InOrder;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.verify;
-
 /**
- * Created by javierg on 19/10/2017.
+ * Created by javierg on 20/10/2017.
  */
 
 public class NotesPresenterTest {
-
-    private static final String TITLE = "title";
-    private static final String DESCRIPTION = "description";
-
-    @Mock
-    private NotesRepository mNotesRepository;
-
-    @Mock
-    private NotesContract.View mNotesView;
-
-    @Captor
-    ArgumentCaptor<NotesRepository.LoadNotesCallback> mLoadNotesCallbackArgumentCaptor;
-
-    NotesPresenter mNotesPresenter;
-
-    List<Note> mNotes;
-
-    @Before
-    public void setUpNotesPresenterTest() {
-        MockitoAnnotations.initMocks(this);
-
-        mNotesPresenter = new NotesPresenter(mNotesRepository, mNotesView);
-        mNotes = new ArrayList<>();
-        mNotes.add(new Note(TITLE, DESCRIPTION));
-        mNotes.add(new Note(TITLE, DESCRIPTION));
-    }
-
-    @Test
-    public void loadNotes_showNotes() {
-
-        boolean forceUpdate = true;
-
-        mNotesPresenter.loadNotes(forceUpdate);
-
-        verify(mNotesRepository).getNotes(mLoadNotesCallbackArgumentCaptor.capture());
-        mLoadNotesCallbackArgumentCaptor.getValue().onNotesLoaded(mNotes);
-
-        InOrder inOrder = Mockito.inOrder(mNotesView);
-        inOrder.verify(mNotesView).setProgressIndicator(true);
-        inOrder.verify(mNotesView).setProgressIndicator(false);
-
-        verify(mNotesView).showNotes(mNotes);
-    }
-
-    @Test
-    public void addNewNote_showAddNote() {
-
-        mNotesPresenter.addNewNote();
-
-        verify(mNotesView).showAddNote();
-    }
-
-    @Test
-    public void openNoteDetails_showNoteDetailUi() {
-
-        Note requestedNote = new Note(TITLE, DESCRIPTION);
-
-        mNotesPresenter.openNoteDetails(requestedNote);
-    }
 }
