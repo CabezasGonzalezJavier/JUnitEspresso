e99843e682e79d889b0dc65e3f72c7c55dd7a664
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-10-23 16:42:00 +0100

    fixed test InMemoryNotesRepositoryTest

--- app/src/test/java/com/dream/myself/InMemoryNotesRepositoryTest.java
+++ app/src/test/java/com/dream/myself/InMemoryNotesRepositoryTest.java
@@ -1,8 +1,67 @@
 package com.dream.myself;
 
+import com.dream.myself.data.InMemoryNotesRepository;
+import com.dream.myself.data.Note;
+import com.dream.myself.data.NotesRepository;
+import com.dream.myself.data.NotesServiceApi;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+
 /**
- * Created by javierg on 20/10/2017.
+ * Created by javierg on 23/10/2017.
  */
 
 public class InMemoryNotesRepositoryTest {
+
+    @Mock
+    private NotesServiceApi mNotesServiceApi;
+
+    @Mock
+    private NotesRepository.LoadNotesCallback mLoadNotesCallback;
+
+    @Mock
+    private NotesRepository.GetNoteCallback mGetNoteCallback;
+
+    InMemoryNotesRepository mInMemoryNotesRepository;
+
+    @Before
+    public void setUpInMemoryNotesRepositoryTest() {
+        MockitoAnnotations.initMocks(this);
+
+        mInMemoryNotesRepository = new InMemoryNotesRepository(mNotesServiceApi);
+    }
+
+    @Test
+    public void getNotes() {
+
+        mInMemoryNotesRepository.getNotes(mLoadNotesCallback);
+
+        verify(mNotesServiceApi).getAllNotes(any(NotesServiceApi.NotesServiceCallback.class));
+
+    }
+
+    @Test
+    public void saveNote() {
+        Note note = new Note("title", "description");
+
+        mInMemoryNotesRepository.saveNote(note);
+
+        verify(mNotesServiceApi).saveNote(any(Note.class));
+    }
+
+    @Test
+    public void getNote() {
+        Note note = new Note("title", "description");
+
+        mInMemoryNotesRepository.getNote(note.getId(), mGetNoteCallback);
+
+        verify(mNotesServiceApi).getNote(eq(note.getId()), any(NotesServiceApi.NotesServiceCallback.class));
+    }
 }
--- app/src/test/java/com/dream/myself/NotesPresenterTest.java
+++ app/src/test/java/com/dream/myself/NotesPresenterTest.java
@@ -1,8 +1,86 @@
 package com.dream.myself;
 
+import com.dream.myself.data.Note;
+import com.dream.myself.data.NotesRepository;
+import com.dream.myself.notes.NotesContract;
+import com.dream.myself.notes.NotesPresenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+
 /**
- * Created by javierg on 20/10/2017.
+ * Created by javierg on 23/10/2017.
  */
 
 public class NotesPresenterTest {
+
+    @Mock
+    private NotesRepository mNotesRepository;
+
+    @Mock
+    private NotesContract.View mNotesView;
+
+    @Captor
+    ArgumentCaptor<NotesRepository.LoadNotesCallback> mLoadNotesCallbackArgumentCaptor;
+
+    NotesPresenter mNotesPresenter;
+
+    private List<Note> mList;
+
+    @Before
+    public void setUpNotesPresenterTest () {
+        MockitoAnnotations.initMocks(this);
+
+        mNotesPresenter = new NotesPresenter(mNotesRepository, mNotesView);
+
+        mList = new ArrayList<>();
+        mList.add(new Note("title", "description"));
+        mList.add(new Note("title", "description"));
+    }
+
+    @Test
+    public void loadNotes_getNotes() {
+
+        boolean forceUpdate = true;
+
+        mNotesPresenter.loadNotes(forceUpdate);
+        verify(mNotesRepository).getNotes(mLoadNotesCallbackArgumentCaptor.capture());
+        mLoadNotesCallbackArgumentCaptor.getValue().onNotesLoaded(mList);
+
+        InOrder inOrder = Mockito.inOrder(mNotesView);
+        inOrder.verify(mNotesView).setProgressIndicator(true);
+        inOrder.verify(mNotesView).setProgressIndicator(false);
+
+        verify(mNotesView).showNotes(mList);
+    }
+
+    @Test
+    public void addNewNote_showAddNote() {
+        mNotesPresenter.addNewNote();
+
+        verify(mNotesView).showAddNote();
+    }
+
+    @Test
+    public void openNoteDetails_showNoteDetailUi() {
+
+        Note note = new Note("title", "description");
+
+        mNotesPresenter.openNoteDetails(note);
+
+        verify(mNotesView).showNoteDetailUi(anyString());
+    }
+
 }
