86c16e8eb515f3f4333305a8ea50db4a220a1790
Cabezas <javiergonzalezcabezas@gmail.com>
2017-12-11 22:55:07 +0000

    added description

--- app/src/test/java/com/dream/myself/AddNotePresenterTest.java
+++ app/src/test/java/com/dream/myself/AddNotePresenterTest.java
@@ -5,7 +5,13 @@ package com.dream.myself;
  */
 
 public class AddNotePresenterTest {
+    /**Given an array of integers arr:
 
+    Write a function flip(arr, k) that reverses the order of the first
+    k elements in the array arr.
+    Write a function pancakeSort(arr) that sorts and returns the input array.
+    You are allowed to use only the function flip you wrote in the first step
+    in order to make changes in the array.**/
     class Solution {
 
         static int[] pancakeSort(int[] arr) {
